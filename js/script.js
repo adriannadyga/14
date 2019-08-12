ee2561b8d2d03d74e981051cfac2703f2051b392
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-08-12 20:45:05 +0200

    Add carousel

---
+++ js/script.js
@@ -0,0 +1,18 @@
+'use strict'
+//flickity
+var carousel = document.querySelector('.main-carousel');
+var carouselStatus = document.querySelector('.carousel-status');
+
+var flkty = new Flickity(carousel, {
+    cellAlign: 'left',
+    contain: true,
+    pageDots: false,
+});
+
+function updateStatus() {
+    var slideNumber = flkty.selectedIndex + 1;
+    carouselStatus.textContent = '0' + slideNumber + '/' + '0' + flkty.slides.length;
+  }
+  updateStatus();
+
+  flkty.on( 'select', updateStatus );
