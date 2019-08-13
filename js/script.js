fa31aadf7bc935a33477cb21e357e91a8376020d
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-08-13 22:04:48 +0200

    Add responsive to carousels

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 'use strict'
 //flickity
-var carousel = document.querySelector('.main-carousel');
-var carouselStatus = document.querySelector('.carousel-status');
+var carousel = document.getElementById('carousel1');
+var carouselStatus = document.getElementById('status');
 
 var flkty = new Flickity(carousel, {
+    cellAlign: 'left',
+    contain: true,
+    pageDots: false
+});
+
+function updateStatus() {
+  var slideNumber = flkty.selectedIndex + 1;
+  carouselStatus.textContent = '0' + slideNumber + '/' + '0' + flkty.slides.length;
+}
+updateStatus();
+
+flkty.on('select', updateStatus);
+
+//flickity
+var carousel2 = document.getElementById('carousel2');
+
+var flkty2 = new Flickity(carousel2, {
     cellAlign: 'left',
     contain: true,
     pageDots: false,
 });
 
-function updateStatus() {
-    var slideNumber = flkty.selectedIndex + 1;
-    carouselStatus.textContent = '0' + slideNumber + '/' + '0' + flkty.slides.length;
-  }
-  updateStatus();
-
-  flkty.on( 'select', updateStatus );
